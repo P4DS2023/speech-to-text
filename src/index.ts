17de9a9ffc1ed351852cbfcb69712e3c5305d5de
Tim Lindenau <tim.lindenau@outlook.de>
2023-10-25 19:34:27 +0900

    update stuff

--- src/index.ts
+++ src/index.ts
@@ -11,25 +11,60 @@ const io = new Server(3001, {
 
 io.on('connection', (socket) => {
   console.log('A user connected');
+
   const speechClient = new SpeechClient();
   const request = {
     config: {
       encoding: 'WEBM_OPUS',
       sampleRateHertz: 16000,
-      languageCode: 'en-US'
+      languageCode: 'en-US',
+      enableAutomaticPunctuation: true,
+      enableWordTimeOffsets: true
     },
     interimResults: true // If you want interim results, set this to true
   };
-
+  let averageSpeedWPM = 0;
   const recognizeStream = speechClient
     .streamingRecognize(request as any)
     .on('error', (err) => console.log(err))
     .on('data', (data) => {
       const isFinal = data.results[0].isFinal;
       const transcript = data.results[0].alternatives[0].transcript;
-      const speechClarity = data.results[0].alternatives[0].confidence;
-    
-      socket.emit('transcript', { transcript, isFinal, speechClarity });
+
+      if (isFinal) {
+        const speechClarity = data.results[0].alternatives[0].confidence;
+        const wordLengthsSeconds = data.results[0].alternatives[0].words.map(
+          (word: any) => {
+            const startTimeSeconds =
+              word.startTime.seconds + word.startTime.nanos * 1e-9;
+            const endTimeSeconds =
+              word.endTime.seconds + word.endTime.nanos * 1e-9;
+            return endTimeSeconds - startTimeSeconds;
+          }
+        );
+        const wordLengths = wordLengthsSeconds.reduce(
+          (acc: number, curr: number) => acc + curr,
+          0
+        );
+        const wordsInBlock = wordLengthsSeconds.length;
+        let averageSpeedWPMCurrent: undefined | number = undefined;
+        if (wordsInBlock === 0) {
+          averageSpeedWPMCurrent = undefined;
+        } else {
+          averageSpeedWPMCurrent = wordsInBlock / (wordLengths / 60);
+          averageSpeedWPM = (averageSpeedWPM + averageSpeedWPMCurrent) / 2;
+        }
+
+        return socket.emit('transcript', {
+          transcript,
+          isFinal,
+          speechClarity,
+          averageSpeedWPMCurrent,
+          averageSpeedWPM
+        });
+      }
+
+      return socket.emit('transcript', { transcript, isFinal });
     })
     .on('end', () => {
       console.log('end');
